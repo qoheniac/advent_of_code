bb4887d9647563588f7cf6f6f1b9821334aa4459
qoheniac <29815186+qoheniac@users.noreply.github.com>
2023-12-13 00:43:28 +0100

    2023 day 5 part 2

--- src/aoc2023/day05.rs
+++ src/aoc2023/day05.rs
@@ -6,7 +6,8 @@
 //! one is the start of the destination range, the second one is the start of
 //! the source range, and the last one is the length of the ranges. If no range
 //! applies, numbers stay unchanged. Successively applying all maps to the seed
-//! numbers gives the respective location numbers
+//! numbers gives the respective location numbers where only the lowest one is
+//! of interest.
 
 // Range of numbers mapping some distance in positive or negative direction
 struct MapRange {
@@ -15,44 +16,15 @@ struct MapRange {
     is_subtracting: bool,
 }
 
-// Mapping a number by trying a number of MapRanges or map to itself
-fn destination(map: &Vec<MapRange>, source: usize) -> usize {
-    for map_range in map {
-        if map_range.range.contains(&source) {
-            return if map_range.is_subtracting {
-                source - map_range.distance
-            } else {
-                source + map_range.distance
-            };
-        }
-    }
-    source
-}
-
 // Return the next number from a string of whitespace-separated numbers
 fn next_number(numbers: &mut std::str::SplitWhitespace) -> Option<usize> {
     numbers.next().and_then(|number| number.parse().ok())
 }
 
-/// Part 1: Lowest location number
-pub fn part1(input: String) -> crate::PuzzleResult {
-    let mut blocks = input.split("\n\n");
-
-    // Parse seeds
-    let seeds: Vec<usize> = blocks
-        .next()
-        .and_then(|line| line.strip_prefix("seeds: "))
-        .and_then(|numbers| {
-            numbers
-                .split_whitespace()
-                .map(|number| number.parse().ok())
-                .collect()
-        })
-        .ok_or("seeds not found")?;
-
-    // Parse maps
+// Parse maps text blocks into a maps vector where each map is MapRange vector
+fn parse_maps(map_blocks: std::str::Split<&str>) -> Result<Vec<Vec<MapRange>>, String> {
     let mut maps = Vec::new();
-    for block in blocks {
+    for block in map_blocks {
         let (name, block) = block.split_once(":\n").ok_or("couldn't parse block")?;
         let error = format!("couldn't parse {name}");
         let mut ranges = Vec::new();
@@ -69,59 +41,154 @@ pub fn part1(input: String) -> crate::PuzzleResult {
         }
         maps.push(ranges);
     }
+    Ok(maps)
+}
+
+/// Part 1: First line lists seeds
+pub fn part1(input: String) -> crate::PuzzleResult {
+    let mut blocks = input.split("\n\n");
+
+    // Parse input
+    let seeds: Vec<usize> = blocks
+        .next()
+        .and_then(|line| line.strip_prefix("seeds: "))
+        .and_then(|numbers| {
+            numbers
+                .split_whitespace()
+                .map(|number| number.parse().ok())
+                .collect()
+        })
+        .ok_or("seeds not found")?;
+    let maps = parse_maps(blocks)?;
 
     // Apply maps and find lowest location
     let mut location = usize::MAX;
     for seed in seeds {
         let mut number = seed;
         for map in &maps {
-            number = destination(map, number)
+            for map_range in map {
+                if map_range.range.contains(&number) {
+                    if map_range.is_subtracting {
+                        number -= map_range.distance;
+                    } else {
+                        number += map_range.distance;
+                    };
+                    break;
+                }
+            }
         }
         location = location.min(number);
     }
     Ok(location.to_string())
 }
 
+/// Part 2: First line lists seed ranges
+pub fn part2(input: String) -> crate::PuzzleResult {
+    let mut blocks = input.split("\n\n");
+
+    // Parse input
+    let mut iter = blocks
+        .next()
+        .and_then(|line| line.strip_prefix("seeds: "))
+        .ok_or("seeds not found")?
+        .split_whitespace();
+    let mut ranges = Vec::<(usize, usize)>::new();
+    while let (Some(start), Some(length)) = (iter.next(), iter.next()) {
+        if let (Ok(start), Ok(length)) = (start.parse(), length.parse()) {
+            ranges.push((start, length));
+        }
+    }
+    let maps = parse_maps(blocks)?;
+
+    // Apply all maps to all seed ranges
+    for map in &maps {
+        let mut mapped_ranges = Vec::new();
+        for (start, length) in ranges {
+            let mut overlaps = Vec::new();
+            for map_range in map {
+                if map_range.range.start < start + length && start < map_range.range.end {
+                    let overlap_start = start.max(map_range.range.start);
+                    let overlap_length = (start + length).min(map_range.range.end) - overlap_start;
+                    overlaps.push((overlap_start, overlap_length));
+
+                    // Map overlap
+                    let mapped_start = if map_range.is_subtracting {
+                        overlap_start - map_range.distance
+                    } else {
+                        overlap_start + map_range.distance
+                    };
+                    mapped_ranges.push((mapped_start, overlap_length));
+                }
+            }
+
+            // Map unmatched regions
+            overlaps.sort();
+            let mut unmapped_start = start;
+            for (overlap_start, overlap_length) in overlaps {
+                if unmapped_start < overlap_start {
+                    mapped_ranges.push((unmapped_start, overlap_start - unmapped_start));
+                }
+                unmapped_start = overlap_start + overlap_length;
+            }
+            if unmapped_start < start + length {
+                mapped_ranges.push((unmapped_start, start + length - unmapped_start));
+            }
+        }
+        ranges = mapped_ranges;
+    }
+
+    // Find lowest location
+    if let Some(min) = ranges.iter().map(|(start, _)| start).min() {
+        Ok(min.to_string())
+    } else {
+        Err("no locations")?
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    const INPUT: &str = "\
+        seeds: 79 14 55 13\n\
+        \n\
+        seed-to-soil map:\n\
+        50 98 2\n\
+        52 50 48\n\
+        \n\
+        soil-to-fertilizer map:\n\
+        0 15 37\n\
+        37 52 2\n\
+        39 0 15\n\
+        \n\
+        fertilizer-to-water map:\n\
+        49 53 8\n\
+        0 11 42\n\
+        42 0 7\n\
+        57 7 4\n\
+        \n\
+        water-to-light map:\n\
+        88 18 7\n\
+        18 25 70\n\
+        \n\
+        light-to-temperature map:\n\
+        45 77 23\n\
+        81 45 19\n\
+        68 64 13\n\
+        \n\
+        temperature-to-humidity map:\n\
+        0 69 1\n\
+        1 0 69\n\
+        \n\
+        humidity-to-location map:\n\
+        60 56 37\n\
+        56 93 4";
 
     #[test]
     fn test_part1() {
-        let input: String = "\
-            seeds: 79 14 55 13\n\
-            \n\
-            seed-to-soil map:\n\
-            50 98 2\n\
-            52 50 48\n\
-            \n\
-            soil-to-fertilizer map:\n\
-            0 15 37\n\
-            37 52 2\n\
-            39 0 15\n\
-            \n\
-            fertilizer-to-water map:\n\
-            49 53 8\n\
-            0 11 42\n\
-            42 0 7\n\
-            57 7 4\n\
-            \n\
-            water-to-light map:\n\
-            88 18 7\n\
-            18 25 70\n\
-            \n\
-            light-to-temperature map:\n\
-            45 77 23\n\
-            81 45 19\n\
-            68 64 13\n\
-            \n\
-            temperature-to-humidity map:\n\
-            0 69 1\n\
-            1 0 69\n\
-            \n\
-            humidity-to-location map:\n\
-            60 56 37\n\
-            56 93 4"
-            .to_string();
-        assert_eq!(&super::part1(input).unwrap(), "35");
+        assert_eq!(&super::part1(INPUT.to_string()).unwrap(), "35");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(&super::part2(INPUT.to_string()).unwrap(), "46");
     }
 }
