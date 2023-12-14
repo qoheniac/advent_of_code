ed53cede5de6110f76389390f896c9f239228c98
qoheniac <29815186+qoheniac@users.noreply.github.com>
2023-12-15 00:40:02 +0100

    KaTeX

--- src/aoc2023/day06.rs
+++ src/aoc2023/day06.rs
@@ -2,17 +2,19 @@
 //!
 //! The first line has a list of integer times and the second line a list of
 //! integer distances. Setting up the velocity takes one time unit for each
-//! velocity unit, that is one distance unit per time unit. Each time value t
-//! gives a total amount of time available and each distance value d a minimum
-//! distance that should be reached. The velocity v necessary to reach exactly
-//! the distance d is
+//! velocity unit, that is one distance unit per time unit. Each time value
+//! \\(t\\) gives a total amount of time available and each distance value
+//! \\(d\\) a minimum distance that should be reached. The velocity \\(v\\)
+//! necessary to reach exactly the distance \\(d\\) is
 //!
-//! >   d = v (t - v)   =>   v± = t/2 ± sqrt(t²/4 - d)
+//! \\[ d = v (t - v) \\quad \\Rightarrow \\quad
+//! v_\\pm = \\tfrac{1}{2} t \\pm \\sqrt{\\tfrac{1}{4} t^2 - d} \\]
 //!
 //! Every velocity between these two velocity solutions will result in a
-//! distance larger than d. Because only integer velocities are allowed, the
-//! smallest velocity to reach further than d is the next integer larger than v-
-//! and the largest one is the next integer smaller than v+.
+//! distance larger than \\(d\\). Because only integer velocities are allowed,
+//! the smallest velocity to reach further than \\(d\\) is the next integer
+//! larger than \\(v_-\\) and the largest one is the next integer smaller than
+//! \\(v_+\\).
 
 fn ways((time, distance): (f64, f64)) -> usize {
     let sqrt = (time * time / 4.0 - distance).sqrt();
