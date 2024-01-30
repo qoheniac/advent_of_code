8420c5a1371d0bccc9627bc691bbd90fff7e7d31
qoheniac <29815186+qoheniac@users.noreply.github.com>
2024-01-30 17:09:48 +0100

    use shoelace formula

--- src/aoc2023/day18.rs
+++ src/aoc2023/day18.rs
@@ -2,8 +2,6 @@
 //!
 //! [puzzle site](https://adventofcode.com/2023/day/18)
 
-use num::Integer;
-
 enum Direction {
     Down,
     Left,
@@ -13,12 +11,12 @@ enum Direction {
 use Direction::*;
 
 impl Direction {
-    fn step(&self, start: &(i32, i32)) -> (i32, i32) {
+    fn go(&self, start: &(i32, i32), distance: i32) -> (i32, i32) {
         match self {
-            Down => (start.0, start.1 - 1),
-            Left => (start.0 - 1, start.1),
-            Right => (start.0 + 1, start.1),
-            Up => (start.0, start.1 + 1),
+            Down => (start.0, start.1 - distance),
+            Left => (start.0 - distance, start.1),
+            Right => (start.0 + distance, start.1),
+            Up => (start.0, start.1 + distance),
         }
     }
 }
@@ -37,97 +35,24 @@ impl std::str::FromStr for Direction {
     }
 }
 
-struct Trench(Vec<(i32, i32)>);
-
-impl Trench {
-    fn index_before(&self, index: usize) -> usize {
-        index.checked_sub(1).unwrap_or(self.0.len() - 1)
-    }
-
-    fn before(&self, index: usize) -> (i32, i32) {
-        self.0[self.index_before(index)]
-    }
-
-    fn index_after(&self, index: usize) -> usize {
-        (index + 1) % self.0.len()
-    }
-
-    fn after(&self, index: usize) -> (i32, i32) {
-        self.0[self.index_after(index)]
-    }
-}
-
 /// Part 1
 pub fn part1(input: String) -> crate::PuzzleResult {
-    // parse trench
-    let mut trench = Trench(vec![(0, 0)]);
+    let mut lagoon_volume = 0;
+    let mut trench_length = 0;
+    let mut location = (0, 0);
     for line in input.lines() {
         let mut split = line.split(' ');
         let error = format!("cannot parse line {line}");
         let direction: Direction = split.next().ok_or(error.clone())?.parse()?;
-        let distance: usize = split.next().ok_or(error)?.parse()?;
-        for _ in 0..distance {
-            trench.0.push(direction.step(trench.0.last().unwrap()));
-        }
+        let distance: i32 = split.next().ok_or(error)?.parse()?;
+        let (x_1, y_1) = location;
+        location = direction.go(&location, distance);
+        let (x_2, y_2) = location;
+        lagoon_volume += x_1 * y_2 - x_2 * y_1;
+        trench_length += distance;
     }
-    let top = *trench.0.iter().map(|(_, y)| y).max().ok_or("empty input")?;
-    if trench.0.len() < 9 || trench.0.pop().unwrap() != trench.0[0] {
-        Err("dig plan did not result in a loop")?;
-    }
-
-    // find point inside
-    let mut point_inside = None;
-    for point in [(1, 1), (1, -1), (-1, -1), (-1, 1)] {
-        if trench.0.contains(&point) {
-            continue;
-        }
-
-        // ray casting
-        let mut count = 0;
-        let mut location = point;
-        while location.1 <= top + 1 {
-            location = Up.step(&location);
-
-            // ray hits trench
-            if let Some(index) = trench.0.iter().position(|&p| p == location) {
-                let mut start_index = index;
-                while trench.before(start_index).0 == location.0 {
-                    start_index = trench.index_before(start_index);
-                    location = Up.step(&location);
-                }
-                let mut end_index = index;
-                while trench.after(end_index).0 == location.0 {
-                    end_index = trench.index_after(end_index);
-                    location = Up.step(&location);
-                }
-
-                // check for intersection (no tangent)
-                if trench.before(start_index).0 != trench.after(end_index).0 {
-                    count += 1;
-                }
-            }
-        }
-        if count.is_odd() {
-            point_inside = Some(point);
-            break;
-        }
-    }
-    let point_inside = point_inside.ok_or("no point inside trench found")?;
-
-    // flood fill
-    let mut interior = std::collections::HashSet::new();
-    let mut queue = std::collections::VecDeque::from([point_inside]);
-    while !queue.is_empty() {
-        let point = queue.pop_front().unwrap();
-        if !trench.0.contains(&point) && interior.insert(point) {
-            for direction in [Down, Left, Right, Up] {
-                queue.push_back(direction.step(&point));
-            }
-        }
-    }
-
-    // count and return total volume
-    Ok((trench.0.len() + interior.len()).to_string())
+    lagoon_volume = (lagoon_volume.abs() + trench_length) / 2 + 1;
+    Ok(lagoon_volume.to_string())
 }
 
 #[cfg(test)]
